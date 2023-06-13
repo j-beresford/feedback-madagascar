2ac320dfc40c3dd291c5c9af0452c7bca2ed7d6c
j-beresford <68725342+j-beresford@users.noreply.github.com>
2023-06-13 22:23:59 +0100

    changes

--- tree_loss.R
+++ tree_loss.R
@@ -2,19 +2,23 @@ rm(list=ls())
 library(httr)
 library(jsonlite)
 library(tidyverse)
+library(plm)        
+library(broom)
 
+#### Set API parameters
 api_key <- "4f55fc97-6141-41ea-8281-630ff9c7c3dc"
-
-
-
-## Get tree loss data
 url <- "https://data-api.globalforestwatch.org/dataset/umd_tree_cover_loss/latest/query"
-
 headers <- c(
   "x-api-key" = api_key,
   "Content-Type" = "application/json"
 )
 
+
+################
+### Get data ###
+################
+
+## Treatment ##
 body <- '{
   "geometry": {
     "type": "Polygon",
@@ -33,21 +37,20 @@ body <- '{
 response <- POST(url, add_headers(headers), body = body)
 content <- content(response, as = "text")
 parsed_content <- fromJSON(content)
+treatment_data <- parsed_content[['data']]%>%
+  mutate(group='treatment')
 
-treatment_data <- parsed_content[['data']]
-
-
-##### Control group data #####
+## Control ##
 body <- '{
   "geometry": {
     "type": "Polygon",
     "coordinates": [
       [
-        [103.29732666015625, 0.4537709801264608],
-        [103.34882507324219, 0.4647567848663363],
-        [103.31277618408203, 0.4932511181408705],
-        [103.29732666015625, 0.4537709801264608]
-      ]
+        [48.36932639981467, -17.681685327509115],
+        [48.36932639981467, -17.92955607376615],
+        [48.870286545981486, -17.92955607376615],
+        [148.870286545981486, -17.681685327509115],
+        [48.36932639981467, -17.681685327509115]      ]
     ]
   },
   "sql": "SELECT SUM(area__ha) FROM results GROUP BY umd_tree_cover_loss__year"
@@ -56,6 +59,27 @@ body <- '{
 response <- POST(url, add_headers(headers), body = body)
 content <- content(response, as = "text")
 parsed_content <- fromJSON(content)
-control_data <- parsed_content[['data']]
+control_data <- parsed_content[['data']]%>%
+  mutate(group='control')
 
+### Clean and merge treatment and control ###
+merged_data <- treatment_data%>%
+  bind_rows(control_data)%>%
+  rename(year=umd_tree_cover_loss__year, area=area__ha)%>%
+  mutate(intervention=if_else(year>2005 & group=='treatment', 1, 0))%>%
+  as_tibble()
+
+#########################
+##### DID analysis ######
+#########################
+
+
+### Convert the data into a indexed panel
+pdata <- pdata.frame(merged_data, index = c("group" ,"year"))
+
+### Run a fixed effects model
+model <- plm(area ~ intervention, data = pdata, model = "within")
+
+## Show model results
+tidy(model)
 
